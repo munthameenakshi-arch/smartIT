a4fb52d4640984fdfc143d7d45e25bb9ffc63edd
2410520047-droid <2410520047@klh.edu.in>
2026-08-12 11:02:18 +0530

    Add home page redirect

--- smartit/urls.py
+++ smartit/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(
+        url="/accounts/login/",
+        permanent=False
+    )),
+
     path("admin/", admin.site.urls),
 
     path("accounts/", include("accounts.urls")),
