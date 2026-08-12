f8f5fb81339c0c677f633c0edeaa9a6bf5734b0c
2410520047-droid <2410520047@klh.edu.in>
2026-08-12 13:36:20 +0530

    Add user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
 
 
 def login_view(request):
@@ -27,6 +27,33 @@ def login_view(request):
     return render(
         request,
         "accounts/login.html",
+        {
+            "form": form
+        }
+    )
+
+
+def register_view(request):
+
+    if request.method == "POST":
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            login(request, user)
+
+            return redirect("dashboard")
+
+    else:
+
+        form = UserCreationForm()
+
+    return render(
+        request,
+        "accounts/register.html",
         {
             "form": form
         }
